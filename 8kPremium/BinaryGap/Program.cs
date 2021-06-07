3fcc0fca6f84854bd5669f0155e3ae629ee20704
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-07 23:23:58 +0200

    Add Zadanie1

---
+++ 8kPremium/BinaryGap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = CountZero.BinaryGap(20);
+        }
+    }
+
+    public class CountZero
+    {
+        public static int BinaryGap(int number)
+        {
+            return 10;
+        }
+    }
+}
