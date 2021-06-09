ac80a3f268a0097cf558a4ad88b5d33a51a4a009
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-09 23:20:28 +0200

    Add project Task1

---
+++ 8kPremium/Task1/ConvertToBinary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    public class ConvertToBinary
+    {
+        public string ConvertNumberToBinary()
+        {
+            Console.WriteLine("Give number: ");
+            string number = Console.ReadLine();
+            int numberToInt = int.Parse(number);
+            var convertedNumber = Convert.ToString(numberToInt, 2);
+            Console.WriteLine("Binary number: \n" + convertedNumber);
+            return convertedNumber;
+        }
+    }
+}
---
+++ 8kPremium/Task1/CountZero.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+{
+    public class CountZero 
+    {
+        public int BinaryGap()
+        {
+            var convert = new ConvertToBinary();
+            convert.ConvertNumberToBinary();
+            return 1;
+        }
+    }
+}
