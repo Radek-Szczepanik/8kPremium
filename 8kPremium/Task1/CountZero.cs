56ac230f07d9d8fccb563f29df656177860d1891
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-12 19:53:57 +0200

    Update Task1

--- 8kPremium/Task1/CountZero.cs
+++ 8kPremium/Task1/CountZero.cs
@@ -1,12 +1,39 @@
-﻿namespace Task1
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Task1
 {
     public class CountZero 
     {
-        public int BinaryGap()
+        int numberOfZero;
+        List<string> count = new List<string>();
+
+        public int BinaryGap(int number)
         {
-            var convert = new ConvertToBinary();
-            convert.ConvertNumberToBinary();
-            return 1;
+            var convertedNumber = Convert.ToString(number, 2);
+
+            Regex rg = new Regex(@"1(0+)1");
+
+            MatchCollection matches = rg.Matches(convertedNumber);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                count.Add(matches[i].Groups[1].Value);
+            }
+            foreach (var item in count)
+            {
+                numberOfZero = item.Length;
+            }
+            if(numberOfZero > 0)
+            {
+                Console.WriteLine(numberOfZero);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+            return numberOfZero;
         }
     }
 }
--- 8kPremium/Task1/Program.cs
+++ 8kPremium/Task1/Program.cs
@@ -1,26 +1,11 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace Task1
+﻿namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string patern = "abc<def>ghi";
-
-            Regex rg = new Regex(@"\<\<(.*?)\>\>");
-
-            ShowCollection(rg.Matches(patern));
-        }
-
-        static void ShowCollection(MatchCollection match)
-        {
-            for (int i = 0; i < match.Count; i++)
-            {
-                Console.Write(match[i]);
-            }
-            Console.WriteLine();
+            var count = new CountZero();
+            count.BinaryGap(1054);            
         }
     }
 }
