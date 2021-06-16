3c36fbdb6ebf8a4f1782d15ce4ab73079bbe53fa
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-16 22:34:19 +0200

    Add project Task2

---
+++ 8kPremium/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountJewelsInStonesLinq("aA", "aAAbbbb");
+        }
+
+        public static int CountJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (jewels.Contains(stones[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int CountJewelsInStonesLinq(string jewels, string stones)
+            => stones.Where(s => jewels.Contains(s)).Count();
+    }
+}
