120769a0a4e6b3d7838e8d3936088307505bc754
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-17 17:36:09 +0200

    Add new project Task3

---
+++ 8kPremium/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static bool IsPangram(string sentence)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            return true;
+        }
+    }
+}
