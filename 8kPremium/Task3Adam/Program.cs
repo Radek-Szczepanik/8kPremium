857fba55b78a9eef4169f5bec9183e8f1a4edf05
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-21 22:52:50 +0200

    Add new project Task3Adam

---
+++ 8kPremium/Task3Adam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3Adam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IsPangram("The quick brown fox jumps over the lazy dog");
+            IsPangramLinq("The quick brown fox jumps over the lazyy dogg");
+        }
+
+        public static bool IsPangram(string sentence)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            int count = 0;
+
+            foreach (char c in alpha)
+            {
+                foreach (char l in sentence.ToLower())
+                {
+                    if (c == l)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if (count == 26)
+                return true;
+            return false;
+        }
+
+        public static bool IsPangramLinq(string sentence)
+        {
+            return sentence.ToLower().Where(ch => char.IsLetter(ch)).GroupBy(ch => ch).Count() == 26;
+        }
+    }
+}
