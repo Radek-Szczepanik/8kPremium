54515f6f7fff93bc9fc1ea48d2a227e04efa0285
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-24 08:17:28 +0200

    Add new project Task5

---
+++ 8kPremium/Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsSumExist(new List<int> { 1, 2, 3, 4 }, 3);
+        }
+
+        public static bool IsSumExist(List<int> numbers, int k)
+        {
+            foreach (var number in numbers)
+            {
+                
+
+            }
+            if (numbers.Take(2).Sum() == k)
+                return true;
+            return false;
+        }
+    }
+}
