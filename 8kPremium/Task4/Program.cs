6d370cb6032d5b314df8020cef715639d11e9063
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-24 10:26:51 +0200

    Update Task4

--- 8kPremium/Task4/Program.cs
+++ 8kPremium/Task4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task4
 {
@@ -6,7 +7,43 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            
+            var tree = new TreeNode();
+            tree.DeepestLeavesSum(tree);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            int sum = 0;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                int count = q.Count;
+                sum = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = q.Dequeue();
+                    sum += node.val;
+                    if (node.left != null) q.Enqueue(node.left);
+                    if (node.right != null) q.Enqueue(node.right);
+                }
+            }
+            return sum;
         }
     }
 }
