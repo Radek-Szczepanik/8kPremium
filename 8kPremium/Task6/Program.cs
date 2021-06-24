64408d641667168bbac0db366cf522cc317d630c
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-24 17:18:30 +0200

    Add new project Task6

---
+++ 8kPremium/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int TitleToNumber(string columnTitle)
+        {
+            return 1;
+        }
+    }
+}
