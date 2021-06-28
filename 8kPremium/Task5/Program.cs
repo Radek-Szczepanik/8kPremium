c2bf6406d172dbee84cdd8f3e99293c70449d664
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-28 21:32:12 +0200

    Update Task5

--- 8kPremium/Task5/Program.cs
+++ 8kPremium/Task5/Program.cs
@@ -7,6 +7,8 @@ namespace Task5
         static void Main(string[] args)
         {
             IsSumExist(new List<int> { 9, 5, 2, 3, 7, 1, 0 }, 3);
+
+            IsSumExist1(new int[] { 9, 3, 0, 0, 5, 7 }, 20);
         }
 
         public static bool IsSumExist(List<int> numbers, int k)
@@ -19,6 +21,21 @@ namespace Task5
                 foreach (var num in numbers)
                 {
                     if ((tempNumber + num) == k)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        // rozwiązanie Adama
+
+        public static bool IsSumExist1(int[] numbers, int value)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (value - numbers[i] == numbers[j])
                         return true;
                 }
             }
