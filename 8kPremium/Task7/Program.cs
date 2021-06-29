0d97150af9190953621419d3c9677fd67910ecba
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-06-29 08:20:21 +0200

    Add new project Task7

---
+++ 8kPremium/Task7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanToInt();
+        }
+
+        public static void RomanToInt()
+        {
+            var numbers = new Dictionary<string, int>();
+            numbers.Add("I", 1);
+            numbers.Add("II", 2);
+            numbers.Add("III", 3);
+
+            Console.WriteLine(numbers["II"]);
+            
+
+            
+            
+        }
+    }
+}
