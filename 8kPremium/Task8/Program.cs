053491e7700840d88b0934047cee9bcf5d761649
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-07-02 08:22:40 +0200

    Add new project Task8

---
+++ 8kPremium/Task8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValid("([)]");
+        }
+
+        public static bool IsValid(string s)
+        {
+            Stack<char> brackets = new Stack<char>();
+            Dictionary<char, char> bracketsPairs = new Dictionary<char, char>()
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
+
+            try
+            {
+                foreach (var item in s)
+                {
+                    if (bracketsPairs.Keys.Contains(item))
+                        brackets.Push(item);
+                    else if (bracketsPairs.Values.Contains(item))
+                    {
+                        if (item == bracketsPairs[brackets.First()])
+                            brackets.Pop();
+                        else
+                            return false;
+                    }
+                    else
+                        continue;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return brackets.Count() == 0 ? true : false;
+        }
+    }
+}
