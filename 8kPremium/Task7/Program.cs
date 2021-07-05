f362cc3cc377d2cab55f3c4cff2bc68a863cea1a
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-07-05 15:20:44 +0200

    Update Task7

--- 8kPremium/Task7/Program.cs
+++ 8kPremium/Task7/Program.cs
@@ -7,31 +7,39 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            RomanToInt();
+            RomanToInt("I");
         }
 
-        public static int RomanToInt()
+        public static int RomanToInt(string s)
         {
-            var numbers = new Dictionary<string, int>
+            if (s == null || s.Length == 0)
+                return 0;
+
+            Dictionary<char, int> dict = new Dictionary<char, int>()
             {
-                { "I", 1 }, { "II", 2 }, { "III", 3 }, { "IV", 4 }, { "V", 5 }, { "VI", 6 }, { "VII", 7 },
-                { "VIII", 8 }, { "IX", 9 }, { "X", 10 }, { "XX", 20}, { "XXX", 30}, { "XL", 40 }, { "L", 50},
-                { "LX", 60 }, { "LXX", 70 }, { "LXXX", 80 }, { "XC", 90 }, { "C", 100 }, { "CC", 200 },
-                { "CCC", 300}, { "CD", 400 }, { "D", 500 }, { "DC", 600 }, { "DCC", 700 }, { "DCCC", 800 },
-                { "CM", 900 }, { "M", 1000}, { "MD", 1500 }, { "MM", 2000 }, { "MMM", 3000 }
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
             };
-            Console.WriteLine("Podaj cyfrę rzymską: ");
-            string userInput = Console.ReadLine().ToUpper();
-            int value = 0;
-            if (numbers.TryGetValue(userInput, out value))
+
+            int sum = dict[s[s.Length - 1]];
+
+            for (int i = s.Length - 2; i >= 0; i--)
             {
-                Console.WriteLine($"{value}");
+                if (dict[s[i]] < dict[s[i + 1]])
+                {
+                    sum -= dict[s[i]];
+                }
+                else
+                {
+                    sum += dict[s[i]];
+                }
             }
-            else
-            {
-                Console.WriteLine("nie znaleziono");
-            }
-            return value;
+            return sum;
         }
     }
 }
